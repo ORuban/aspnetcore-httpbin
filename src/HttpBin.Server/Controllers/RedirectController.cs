4f3eb49db35813a3890fc50dc7b3faef594982bd
oruban <settlers.net@gmail.com>
2017-11-08 23:19:57 +0200

    add /redirect-to and /temporaryredirect-to endpoints

---
+++ src/HttpBin.Server/Controllers/RedirectController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace HttpBin.Server.Controllers
+{
+    public class RegirectController : Controller
+    {
+        [HttpGet("/redirect-to")]
+        public IActionResult RedirectTo([FromQuery] string url)
+        {
+            return Redirect(url);
+        }
+
+        [HttpGet("/temporaryredirect-to")]
+        public ActionResult TemporaryRedirectTo([FromQuery] string url)
+        {
+            HttpContext.Response.Headers.Add(HeaderNames.Location, url);
+            return new StatusCodeResult(307);
+        }
+    }
+}
