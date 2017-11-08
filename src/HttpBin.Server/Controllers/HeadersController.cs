00e8b2a76084fba73305d662b7c0559948712d3b
oruban <settlers.net@gmail.com>
2017-11-08 22:36:39 +0200

    add /user-agent endpoint

--- src/HttpBin.Server/Controllers/HeadersController.cs
+++ src/HttpBin.Server/Controllers/HeadersController.cs
@@ -1,10 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace HttpBin.Server.Controllers
 {
     public class HeadersController : Controller
     {
+        [HttpGet("/user-agent")]
+        public IActionResult UserAgent()
+        {
+            var headers = HttpContext.Request.Headers;
+            string userAgent = string.Empty;
+
+            if (headers.ContainsKey("User-Agent"))
+            {
+                userAgent = headers["User-Agent"].ToString();
+            }
+
+            var obj = new JObject();
+            obj.Add("user-agent", userAgent);
+
+            return new JsonResult(obj, new JsonSerializerSettings() { Formatting = Formatting.Indented });
+        }
+
         [HttpGet("/headers")]
         public IActionResult Headers()
         {
