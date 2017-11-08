0e68f5e0ecaa82b1df9efc929e5ab7179ff1c0a9
oruban <settlers.net@gmail.com>
2017-11-08 23:00:40 +0200

    add /uuid endpoint

---
+++ src/HttpBin.Server/Controllers/RandomDataController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HttpBin.Server.Controllers
+{
+    public class RandomDataController : Controller
+    {
+        [HttpGet("/uuid")]
+        public IActionResult UUID()
+        {
+            return new JsonResult(new { uuid = Guid.NewGuid() },
+                                  new JsonSerializerSettings() { Formatting = Formatting.Indented });
+        }
+    }
+}
