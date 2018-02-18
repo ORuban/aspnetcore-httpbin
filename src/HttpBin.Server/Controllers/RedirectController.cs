ee2a658e2e7657c14523da758dd08cc1695b3df8
oruban <settlers.net@gmail.com>
2018-02-18 19:58:47 +0200

    update `redirect` endpoints

--- src/HttpBin.Server/Controllers/RedirectController.cs
+++ src/HttpBin.Server/Controllers/RedirectController.cs
@@ -11,11 +11,44 @@ namespace HttpBin.Server.Controllers
             return Redirect(url);
         }
 
-        [HttpGet("/temporaryredirect-to")]
+        [HttpGet("/temporary-redirect-to")]
         public ActionResult TemporaryRedirectTo([FromQuery] string url)
         {
             Response.Headers.Add(HeaderNames.Location, url);
             return new StatusCodeResult(307);
         }
+
+        [HttpGet("/permanent-redirect-to")]
+        public ActionResult PermanentRedirectTo([FromQuery] string url, bool preserveMethod = false)
+        {
+            if (preserveMethod) return RedirectPermanentPreserveMethod(url);
+
+            return RedirectPermanent(url);
+
+        }
+
+        [HttpGet("/relative-redirects/{number}")]
+        public IActionResult RelativeRedirects([FromRoute] int number)
+        {
+            if (number <= 1) return Redirect("/get");
+
+            return Redirect($"/relative-redirects/{number-1}");
+        }  
+
+        [HttpGet("/relative-redirects/{number}")]
+        public IActionResult TemporaryRedirects([FromRoute] int number)
+        {
+            if (number <= 1) return Redirect("/get");
+
+            return Redirect($"/relative-redirects/{number-1}");
+        }   
+
+        [HttpGet("/permanent-redirects/{number}")]
+        public IActionResult PermanentRedirects([FromRoute] int number)
+        {
+            if (number <= 1) return RedirectPermanent("/get");
+
+            return RedirectPermanent($"/permanent-redirects/{number-1}");
+        }   
     }
 }
