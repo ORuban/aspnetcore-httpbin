f9b07d32a9d7d1719edbcf6e416573270aa5f4c4
oruban <settlers.net@gmail.com>
2018-03-10 21:01:42 +0200

    add /post, /put, /patch endpoints

--- src/HttpBin.Server/Controllers/RequestDetailsController.cs
+++ src/HttpBin.Server/Controllers/RequestDetailsController.cs
@@ -2,23 +2,32 @@ using HttpBin.Server.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace HttpBin.Server.Controllers
 {
     public class RequestDetailsController : Controller
     {
         [HttpGet("/get")]
+        [HttpPost("/post")]
+        [HttpPut("/put")]
+        [HttpPatch("/patch")]
         public IActionResult Get()
         {
             string requestUrl = $"{Request.Scheme}://{Request.Host}{Request.Path}{Request.QueryString}";
+            
+            var result = new Dictionary<string, object>();
+            result.Add("args", Request.Query.ToJObject());
+            result.Add("headers", Request.Headers.ToJObject());
+            result.Add("url", requestUrl);
+            result.Add("cookies", Request.Cookies.ToJObject());
 
-            var result = new {
-                args = Request.Query.ToJObject(),
-                headers = Request.Headers.ToJObject(),
-                url = requestUrl,
-                cookies = Request.Cookies.ToJObject()
-            };
-
+            if (Request.Method != "GET")
+            {
+                result.Add("form-data", Request.Form.ToJObject());
+                result.Add("files", Request.Form.Files.ToJObject());
+            }
+            
             return new JsonResult(result, new JsonSerializerSettings() { Formatting = Formatting.Indented });
         }
     }
--- src/HttpBin.Server/Extensions/ConvertingExtensions.cs
+++ src/HttpBin.Server/Extensions/ConvertingExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json.Linq;
@@ -29,6 +30,24 @@ namespace HttpBin.Server.Extensions
             }
 
             return result;
+        }
+
+        public static JObject[] ToJObject(this IEnumerable<IFormFile> collection)
+        {
+            var result = new List<JObject>(collection.Count());
+            
+            foreach(var file in collection)
+            {
+                JObject fileData = new JObject();
+                
+                fileData.Add("fileName", file.FileName);
+                fileData.Add("length", file.Length);
+                fileData.Add("contentType", file.ContentType);
+
+                result.Add(fileData);
+            }
+
+            return result.ToArray<JObject>();
         }
     }
 }
